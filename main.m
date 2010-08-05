2692594408fb814863890dad468431b641e6913e
Cédric Luthi <cedric.luthi@gmail.com>
2010-08-05 10:38:15 +0200

    Fix NSAutoreleaseNoPool warnings

--- main.m
+++ main.m
@@ -16,12 +16,13 @@ CGFloat UIScreen_scale(id self, SEL _cmd)
 
 int main(int argc, char *argv[])
 {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
 	// -[UIView alpha] has the same method signature as -[UIScreen scale]
 	Method alpha = class_getInstanceMethod([UIView class], @selector(alpha));
 	if (![UIScreen instancesRespondToSelector:@selector(scale)])
 		class_addMethod([UIScreen class], @selector(scale), (IMP)UIScreen_scale, method_getTypeEncoding(alpha));
-	
-	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
 	int retVal = UIApplicationMain(argc, argv, nil, nil);
 	[pool release];
 	return retVal;
